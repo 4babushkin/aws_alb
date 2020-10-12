69541ffbd5d5dfe5720481e8e5b393920acc9ea2
Vladimir Babushkin <4babushkin@gmail.com>
2020-10-12 16:52:57 +0700

    create infra with terraform

---
+++ readme.md
@@ -0,0 +1,2 @@
+ AWS Application Load Balancer 
+
---
+++ terraform/variables.tf
@@ -0,0 +1,15 @@
+### main
+variable aws_access_key {
+  description = "AWS Access key:"
+}
+
+variable aws_secret_key {
+  description = "AWS Secret key:"
+}
+variable key_name {
+  default = "babushkin"
+}
+
+variable aws_region {
+  default = "eu-west-1"
+}
